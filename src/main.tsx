4ea528ec1c22de8fb19b657408561f9c88458aa6
ljferreyrac <lferreyrac04@hotmail.com>
2025-03-20 23:46:36 -0500

    feat: add prop-types

--- src/main.tsx
+++ src/main.tsx
@@ -29,7 +29,7 @@ initializeApp(firebaseConfig);
 
 startTraduction();
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.VITE_ENV === "production";
 
 createRoot(document.getElementById("root")!).render(
   <CacheBuster
